f4a20e124d4e460f4f50a7ba4f15cf26a74636a2
Tasfiqul Asif <tasfiq.asif.23@gmail.com>
2024-09-17 00:11:42 +0600

    table for reservation

--- src/pages/ReservationRequest/ReservationRequest.jsx
+++ src/pages/ReservationRequest/ReservationRequest.jsx
@@ -2,42 +2,62 @@
 import { useEffect, useState } from "react";
 import { axiosPublic } from "../../hooks/useAxiosPublic";
 import Header from "../../components/Header/Header";
-import { Paper, Table, TableBody, TableCell, TableContainer, TableHead } from "@mui/material";
+import { IconButton, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 
 const ReservationRequest = () => {
 
-    const [data,setData] = useState([])
+    const [reservationList, setReservationList] = useState([]);
 
     useEffect(()=>{
         const fetchedData =async()=>{
             const {data} = await axiosPublic("/reservation-request");
-            setData(data)
+            setReservationList(data)
         }
         fetchedData()
 
 },[])
-console.log(data)
+
+
+const handleDelete = () => {
+    console.log("delete")
+}
 
     return (
-        <div>
-            <Header text={"Manage Reservations"}/>
-            <TableContainer component={Paper} sx={{marginTop:4}}>
-                <Table>
-                    <TableHead>
-                        <TableCell>Name</TableCell>
-                        <TableCell>Email</TableCell>
-                        <TableCell>Date</TableCell>
-                        <TableCell>Time</TableCell>
-                        <TableCell>Guest</TableCell>
-                        <TableCell>Status</TableCell>
-                        <TableCell>Cancel</TableCell>
-                    </TableHead>
-                    <TableBody></TableBody>
-                </Table>
-
-            </TableContainer>
-        </div>
+      <div>
+        <Header text={"Manage Reservations"} />
+        <TableContainer component={Paper} sx={{ marginTop: 4 }}>
+          <Table>
+            <TableHead>
+              <TableCell>Name</TableCell>
+              <TableCell>Email</TableCell>
+              <TableCell>Date</TableCell>
+              <TableCell>Time</TableCell>
+              <TableCell>Guest</TableCell>
+              <TableCell>Status</TableCell>
+              <TableCell>Cancel</TableCell>
+            </TableHead>
+            <TableBody >
+              {reservationList.map((reservation) => (
+                <TableRow key={reservation._id}>
+                  <TableCell>{reservation.name}</TableCell>
+                  <TableCell>{reservation.email}</TableCell>
+                  <TableCell>{reservation.date}</TableCell>
+                  <TableCell>{reservation.time}</TableCell>
+                  <TableCell>{reservation.guest}</TableCell>
+                  <TableCell>{reservation.status}</TableCell>
+                  <TableCell>
+                    <IconButton onClick={() => handleDelete(reservation._id)}>
+                      <DeleteIcon />
+                    </IconButton>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
     );
 };
 
